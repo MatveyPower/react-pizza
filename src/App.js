c6d5c4cd1f82cc403e3e9d7a9eef7c0c09173c1d
MatveyPower <55589736+MatveyPower@users.noreply.github.com>
2020-12-25 17:42:58 +0300

    рендер компонентов

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from "react"
-
-
+import React, { useState, useEffect } from "react"
+import axios from 'axios'
 import "./scss/app.scss"
 
 import Header from "./components/Header"
-import Home from './pages/Home'
-import Cart from './pages/Cart'
+import Home from "./pages/Home"
+import Cart from "./pages/Cart"
 import { Route } from "react-router-dom"
 
-
 function App() {
-    
+    const [pizzas, setPizzas] = useState([])
 
-
+    useEffect(() => {
+        axios.get("http://localhost:3000/db.json").then(({data}) =>{
+            setPizzas(data.pizzas)
+        }) 
+    }, [])
 
     return (
         <div className="wrapper">
             <Header />
             <div className="content">
-                <Route path= '/' component= {Home} exact />
-                <Route path= '/cart' component= {Cart} exact />
+                <Route path="/" render = {() => <Home items = {pizzas}/>} exact />
+                <Route path="/cart" component={Cart} exact />
             </div>
         </div>
     )
