12f0ee544cfa511e7199b920253e802c5061668a
MatveyPower <55589736+MatveyPower@users.noreply.github.com>
2020-12-25 15:29:27 +0300

    настроил роуты

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,13 +3,17 @@ import React, { useState } from "react"
 export default function Categories({ items, onClickItem }) {
     const [activeItem, setActiveItem] = useState(null)
 
+
+    const onSelectItem = (index ) => {
+        setActiveItem(index)
+    }
     return (
         <div>
             <div className="categories">
                 <ul>
                     <li
                         className={activeItem === null ? "active" : ""}
-                        onClick={() => setActiveItem(null)}
+                        onClick={() => onSelectItem(null)}
                     >
                         Все
                     </li>
@@ -17,7 +21,7 @@ export default function Categories({ items, onClickItem }) {
                         items.map((name, index) => (
                             <li
                                 className={activeItem === index ? "active" : ""}
-                                onClick={() => setActiveItem(index)}
+                                onClick={() => onSelectItem(index)}
                                 key={`${name}_${index}`}
                             >
                                 {name}
