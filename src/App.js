e1b859e685c4c6767f4475183f1762e4226568d6
MatveyPower <55589736+MatveyPower@users.noreply.github.com>
2020-12-25 12:09:18 +0300

    настрйка категорий

--- src/App.js
+++ src/App.js
@@ -2,26 +2,29 @@ import React from "react"
 
 import "./scss/app.scss"
 
-import Header from "./Header"
-
+import Header from "./components/Header"
+import Categories from "./components/Categories"
 
 function App() {
+    
+
+
+
     return (
         <div className="wrapper">
             <Header />
             <div className="content">
                 <div className="container">
                     <div className="content__top">
-                        <div className="categories">
-                            <ul>
-                                <li className="active">Все</li>
-                                <li>Мясные</li>
-                                <li>Вегетарианская</li>
-                                <li>Гриль</li>
-                                <li>Острые</li>
-                                <li>Закрытые</li>
-                            </ul>
-                        </div>
+                        <Categories onClickItem = {(name) => console.log(name)}
+                            items={[
+                                "Мясные",
+                                "Вегетарианская",
+                                "Гриль",
+                                "Острые",
+                                "Закрытые",
+                            ]}
+                        />
                         <div className="sort">
                             <div className="sort__label">
                                 <svg
